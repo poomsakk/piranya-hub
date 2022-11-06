ebaa83187549bdda795237b2b167009df1f01834
Warapob <warapob2544@gmail.com>
2022-11-06 19:34:45 +0700

    Try to connect with backend : Use useState and axios but still have some troubles with axios.post path

--- frontend/src/Register/UserSignIn.js
+++ frontend/src/Register/UserSignIn.js
@@ -1,26 +1,52 @@
 import React, { useState } from 'react'
 import "./UserSignIn.css"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
+import { setUseProxies } from 'immer';
 
 function UserSignIn() {
+
+  let navigate = useNavigate()
+
+  const [user, setUser] = useState({
+    username: "",
+    password: ""
+  })
+
+  const { username, password } = user
+
+  const onInputChange = (e) => {
+    console.log(e)
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // TODO 
+    // Set the path for the submission
+    // await axios.post("", user)
+    navigate("/")
+  }
 
   return (
     <>
       <div className='items-center '>
         <div className='usersignincontent'>
-          <div className='flex flex-row'>
-            <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-10 w-40 " >Email :</h1>
-            <input
-              type={"text"}
-              className="
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className='flex flex-row'>
+              <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-10 w-40 " >Username:</h1>
+              <input
+                type={"text"}
+                className="
                     font-IBMPlexSansThai 
                     bg-[#EFEFEF]
                     placeholder:text-zinc-500
                     text-lg 
                     pl-5 
-                    w-[106%] 
+                    w-[100%] 
                     m-4 
                     ml-20 
                     mt-6 
@@ -28,15 +54,17 @@ function UserSignIn() {
                     border-[#162B78] 
                     rounded-xl 
                     text-m-4"
-              placeholder="Email"
-              name="name"
-            />
-          </div>
-          <div className='flex flex-row'>
-            <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Password :</h1>
-            <input
-              type={"text"}
-              className="
+                placeholder="Username/Email"
+                name="username"
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className='flex flex-row'>
+              <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Password:</h1>
+              <input
+                type={"text"}
+                className="
                     font-IBMPlexSansThai 
                     bg-[#EFEFEF]
                     placeholder:text-zinc-500
@@ -50,14 +78,16 @@ function UserSignIn() {
                     border-[#162B78] 
                     rounded-xl text-m-4 
                     focus:border-[#162B78]"
-              placeholder="Password"
-              name="name"
-            />
-          </div>
-          <div className='flex justify-center ml-5 mr-5 mt-8' >
-            <a href="/information">
-              <button href='/information'
-                class="
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className='flex justify-center ml-5 mr-5 mt-8' >
+              <a href="/information">
+                <button href='/information'
+                  class="
                           bg-[#162B78] 
                           font-IBMPlexSansThai
                           hover:bg-white 
@@ -69,18 +99,19 @@ function UserSignIn() {
                           border border-[#162B78]
                           shadow-md shadow-[#162B78]
                           ">
-                เข้าสู่ระบบ
-              </button>
-            </a>
-          </div>
-          <div className='flex justify-center ' >
-            <a href="/sign-up">
-              <button href='/sign-up'
-                class="signup font-IBMPlexSansThai py-4 px-4">
-                สมัครบัญชี
-              </button>
-            </a>
-          </div>
+                  เข้าสู่ระบบ
+                </button>
+              </a>
+            </div>
+          </form>
+            <div className='flex justify-center ' >
+              <a href="/sign-up">
+                <button href='/sign-up'
+                  class="signup font-IBMPlexSansThai py-4 px-4">
+                  สมัครบัญชี
+                </button>
+              </a>
+            </div>
         </div>
       </div>
     </>
--- frontend/src/Register/UserSignUp.js
+++ frontend/src/Register/UserSignUp.js
@@ -1,20 +1,46 @@
 import React, { useState } from 'react'
 import "./UserSignIn.css"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 
 function UserSignUp() {
+    let navigate = useNavigate()
 
+    const [user, setUser] = useState({
+        username: "",
+        nameSurname: "",
+        email: "",
+        password: ""
+    })
+
+    const { username, nameSurname, email, password } = user
+
+    const onInputChange = (e) => {
+        console.log(e)
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        // TODO 
+        // Set the path for the submission
+        // await axios.post("", user)
+        navigate("/")
+    }
+    
     return (
         <>
             <div className='items-center '>
                 <div className='usersignincontent'>
-                    <div className='flex flex-row'>
-                        <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Username :</h1>
-                        <input
-                            type={"text"}
-                            className="
+                    <form onSubmit={(e) => onSubmit(e)}>
+                        <div className='flex flex-row'>
+                            <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Username :</h1>
+                            <input
+                                type={"text"}
+                                className="
                                     font-IBMPlexSansThai 
                                     bg-[#EFEFEF]
                                     placeholder:text-zinc-500
@@ -28,15 +54,17 @@ function UserSignUp() {
                                     border-[#162B78] 
                                     rounded-xl text-m-4 
                                     focus:border-[#162B78]"
-                            placeholder="Username"
-                            name="name"
-                        />
-                    </div>
-                    <div className='flex flex-row'>
-                        <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Name-Surname :</h1>
-                        <input
-                            type={"text"}
-                            className="
+                                placeholder="Username"
+                                name="username"
+                                value={username}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+                        <div className='flex flex-row'>
+                            <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Name-Surname :</h1>
+                            <input
+                                type={"text"}
+                                className="
                                     font-IBMPlexSansThai 
                                     bg-[#EFEFEF]
                                     placeholder:text-zinc-500
@@ -50,15 +78,17 @@ function UserSignUp() {
                                     border-[#162B78] 
                                     rounded-xl text-m-4 
                                     focus:border-[#162B78]"
-                            placeholder="Name-Surname"
-                            name="name"
-                        />
-                    </div>
-                    <div className='flex flex-row'>
-                        <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Email :</h1>
-                        <input
-                            type={"text"}
-                            className="
+                                placeholder="Name-Surname"
+                                name="nameSurname"
+                                value={nameSurname}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+                        <div className='flex flex-row'>
+                            <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Email :</h1>
+                            <input
+                                type={"text"}
+                                className="
                                     font-IBMPlexSansThai 
                                     bg-[#EFEFEF]
                                     placeholder:text-zinc-500
@@ -72,15 +102,17 @@ function UserSignUp() {
                                     border-[#162B78] 
                                     rounded-xl text-m-4 
                                     focus:border-[#162B78]"
-                            placeholder="Email"
-                            name="name"
-                        />
-                    </div>
-                    <div className='flex flex-row'>
-                        <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Password :</h1>
-                        <input
-                            type={"text"}
-                            className="
+                                placeholder="Email"
+                                name="email"
+                                value={email}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+                        <div className='flex flex-row'>
+                            <h1 className="font-IBMPlexSansThai text-2xl text-[#162B78] m-4 mt-8 w-64 " >Password :</h1>
+                            <input
+                                type={"text"}
+                                className="
                                     font-IBMPlexSansThai 
                                     bg-[#EFEFEF]
                                     placeholder:text-zinc-500
@@ -94,14 +126,16 @@ function UserSignUp() {
                                     border-[#162B78] 
                                     rounded-xl text-m-4 
                                     focus:border-[#162B78]"
-                            placeholder="Password"
-                            name="name"
-                        />
-                    </div>
-                    <div className='flex justify-center ml-5 mr-5 mt-8' >
-                        <a href="/facility">
-                            <button href='/facility'
-                                class="
+                                placeholder="Password"
+                                name="password"
+                                value={password}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+                        <div className='flex justify-center ml-5 mr-5 mt-8' >
+                            <a href="/facility">
+                                <button href='/facility'
+                                    class="
                                     bg-[#162B78] 
                                     font-IBMPlexSansThai
                                     hover:bg-white 
@@ -113,10 +147,11 @@ function UserSignUp() {
                                     border border-[#162B78]
                                     shadow-md shadow-[#162B78]
                             ">
-                                ลงทะเบียน
-                            </button>
-                        </a>
-                    </div>
+                                    ลงทะเบียน
+                                </button>
+                            </a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
