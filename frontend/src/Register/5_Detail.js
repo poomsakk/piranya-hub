d9cef1c06ebad9ea8b11d81d62ca2d0ad2c81e35
Watanai1245 <63010846@kmitl.ac.th>
2022-11-07 20:18:23 +0700

    fix(register)change layout

--- frontend/src/Register/5_Detail.js
+++ frontend/src/Register/5_Detail.js
@@ -1,82 +1,14 @@
 import React from 'react'
 import "./5_Detail.css"
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
 
 function detail() {
-  const steps = [
-    '', '', '', '', '', '', '', '',
-  ];
-  return (
-    <>
-      <div className='container'>
-        <Stepper activeStep={4} alternativeLabel
-          className='py-5 mt-7 '>
-          {steps.map((label) => (
-            <Step key={label}
-              sx={{
-                '& .MuiStepLabel-root .Mui-completed': {
-                  color: '#162B78', // circle color (COMPLETED)
-                },
-                '& .MuiStepLabel-label.Mui-completed.MuiStepLabel-alternativeLabel':
-                {
-                  color: '#EFEFEF', // Just text label (COMPLETED)
-                },
-                '& .MuiStepLabel-root .Mui-active': {
-                  color: '#162B78', // circle color (ACTIVE)
-                },
-                '& .MuiStepLabel-label.Mui-active.MuiStepLabel-alternativeLabel':
-                {
-                  color: '#EFEFEF', // Just text label (ACTIVE)
-                },
-                '& .MuiStepLabel-root .Mui-active .MuiStepIcon-text': {
-                  fill: '#EFEFEF', // circle's number (ACTIVE)
-                },
-              }}>
-              <StepLabel >{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-      </div>
-
-      <div className='flex justify-between ml-5 mr-5 mt-8' >
-        <a href="/cost">
-          <button class="
-                  bg-[#162B78] 
-                  font-IBMPlexSansThai 
-                  hover:bg-white 
-                  hover:text-[#162B78] 
-                  text-white 
-                  py-2 
-                  px-4 
-                  rounded-full 
-                  border border-[#162B78]
-                  shadow-md shadow-[#162B78]
-                  ">
-            กลับ
-          </button>
-        </a>
-        <a href="/image">
-          <button
-            class="
-                  bg-[#162B78] 
-                  font-IBMPlexSansThai
-                  hover:bg-white 
-                  hover:text-[#162B78] 
-                  text-white 
-                  py-2 
-                  px-4 
-                  rounded-full 
-                  border border-[#162B78]
-                  shadow-md shadow-[#162B78]
-                  ">
-            ถัดไป
-          </button>
-        </a>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='container'>
+              <h1 className = "font-IBMPlexSansThai text-2xl text-[#162B78] ml-12 m-2 w-96" >5. รายละเอียด</h1> 
+            </div>
+        </>
+    )
 }
 
 export default detail
---
+++ frontend/src/Register/total.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./total.css"
+import Information from './1_Information'
+import Facility from './2_Facility'
+import Type from './3_Type'
+import Cost from './4_Cost'
+import Detail from './5_Detail'
+import Image from './6_Image'
+import Promotion from './7_Promotion'
+import Contact from './8_Contact'
+
+
+function AddLodge() {
+    
+  return (
+    <div className="background flex justify-center">
+        <div className="flex flex-col">
+            <div className='max-w-[960px] bg-[#fff] flex flex-col mt-10 rounded-3xl'>
+                <div className="mb-10"><Information /></div>
+                <div className="mb-10"><Facility /></div>
+                <div className="mb-10"><Type /></div>
+                <div className="mb-10"><Cost /></div>
+                <div className="mb-10"><Detail /></div>
+                <div className="mb-10"><Image /></div>
+                <div className="mb-10"><Promotion /></div>
+                <div className="mb-10"><Contact /></div>
+            </div>
+            <div className='flex justify-end m-5' >
+                    <a href="/Page404">
+                        <button
+                            class="
+                            bg-[#162B78] 
+                            font-IBMPlexSansThai
+                            hover:bg-white 
+                            hover:text-[#162B78] 
+                            text-white 
+                            py-2 
+                            px-4 
+                            rounded-full 
+                            border border-[#162B78]
+                            shadow-md shadow-[#162B78]
+                            ">
+                            บันทึก
+                        </button>
+                    </a>
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddLodge
